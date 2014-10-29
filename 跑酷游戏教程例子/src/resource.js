f427cf84fc59213ab798e94c404866362033763e
lvsheng <lvsheng@baidu.com>
2014-10-29 10:26:20 +0800

    for save (本来想用animation缓存从plist直接读动画的，但才发现貌似texturePacker生成的plist里不包含动画效果~，所以放弃了~)

--- 跑酷游戏教程例子/src/resource.js
+++ 跑酷游戏教程例子/src/resource.js
@@ -1,16 +1,16 @@
 var res = {
-    helloBG_png : "res/helloBG.png",
-    start_n_png : "res/start_n.png",
-    start_s_png : "res/start_s.png",
-    PlayBG_png  : "res/PlayBG.png",
-    runner_png  : "res/runner.png"
+    helloBG_png: "res/helloBG.png",
+    start_n_png: "res/start_n.png",
+    start_s_png: "res/start_s.png",
+    PlayBG_png: "res/PlayBG.png",
+    runner_png: "res/runner.png",
+    running_png: "res/running.png",
+    running_plist: "res/running.plist"
 };
 
-var g_resources = [
-    //image
-    res.helloBG_png,
-    res.start_n_png,
-    res.start_s_png,
-    res.PlayBG_png,
-    res.runner_png
-];
+var g_resources = [];
+(function () {
+    for (var name in res) {
+        g_resources.push(res[name]);
+    }
+})();
--- 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
+++ 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
@@ -16,10 +16,52 @@ var AnimationLayer = cc.Layer.extend({
     _initView: function () {
         var self = this;
 
-        //cerate the hero sprite
+        //create the runner sprite
         var runner = new cc.Sprite(res.runner_png);
         self._view.runner = runner;
         runner.attr({x: 80, y: 85});
-        this.addChild(runner);
+        self.addChild(runner);
+
+        //create runner animation
+        cc.animationCache.addAnimations(res.running_plist);
+        var runningAnimation = cc.animationCache.getAnimation('running');
+        debugger;
+        var runningAnimateAction = new cc.RepeatForever(new cc.Animate(runningAnimation));
+        self._view.runner.runAction(runningAnimateAction);
     }
 });
+/*
+var AnimationLayer = cc.Layer.extend({
+    spriteSheet:null,
+    runningAction:null,
+    sprite:null,
+    ctor:function () {
+        this._super();
+        this.init();
+    },
+
+    init:function () {
+        this._super();
+
+// create sprite sheet
+        cc.spriteFrameCache.addSpriteFrames(res.running_plist);
+        this.spriteSheet = cc.SpriteBatchNode.create(res.running_png);
+        this.addChild(this.spriteSheet);
+
+// init runningAction
+        var animFrames = [];
+        for (var i = 1; i < 5; i++) {
+            var str = "runner" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(str);
+            animFrames.push(frame);
+        }
+
+        var animation = cc.Animation.create(animFrames, 0.1);
+        this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
+        this.sprite = cc.Sprite.create("#runner1.png");
+        this.sprite.attr({x:80, y:85});
+        this.sprite.runAction(this.runningAction);
+        this.spriteSheet.addChild(this.sprite);
+    }
+});
+    */
