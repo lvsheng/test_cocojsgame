ee990360624b8aaac5357e1be36a9ff821ee29a1
lvsheng <lvsheng@baidu.com>
2014-10-29 11:05:33 +0800

    加了小人的跑动动画

--- 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
+++ 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
@@ -23,45 +23,8 @@ var AnimationLayer = cc.Layer.extend({
         self.addChild(runner);
 
         //create runner animation
-        cc.animationCache.addAnimations(res.running_plist);
-        var runningAnimation = cc.animationCache.getAnimation('running');
-        debugger;
+        var runningAnimation = myUtil.createAnimationWithPlistFileName(res.running_plist, 0.1);
         var runningAnimateAction = new cc.RepeatForever(new cc.Animate(runningAnimation));
         self._view.runner.runAction(runningAnimateAction);
     }
 });
-/*
-var AnimationLayer = cc.Layer.extend({
-    spriteSheet:null,
-    runningAction:null,
-    sprite:null,
-    ctor:function () {
-        this._super();
-        this.init();
-    },
-
-    init:function () {
-        this._super();
-
-// create sprite sheet
-        cc.spriteFrameCache.addSpriteFrames(res.running_plist);
-        this.spriteSheet = cc.SpriteBatchNode.create(res.running_png);
-        this.addChild(this.spriteSheet);
-
-// init runningAction
-        var animFrames = [];
-        for (var i = 1; i < 5; i++) {
-            var str = "runner" + i + ".png";
-            var frame = cc.spriteFrameCache.getSpriteFrame(str);
-            animFrames.push(frame);
-        }
-
-        var animation = cc.Animation.create(animFrames, 0.1);
-        this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
-        this.sprite = cc.Sprite.create("#runner1.png");
-        this.sprite.attr({x:80, y:85});
-        this.sprite.runAction(this.runningAction);
-        this.spriteSheet.addChild(this.sprite);
-    }
-});
-    */
