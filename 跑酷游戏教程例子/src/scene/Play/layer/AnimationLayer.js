6335a4fc8f9d6642c11a41fb77ff4287e22c7b95
lvsheng <lvsheng@baidu.com>
2014-10-29 11:25:26 +0800

    test stopAction,shedule

--- 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
+++ 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
@@ -26,5 +26,9 @@ var AnimationLayer = cc.Layer.extend({
         var runningAnimation = myUtil.createAnimationWithPlistFileName(res.running_plist, 0.1);
         var runningAnimateAction = new cc.RepeatForever(new cc.Animate(runningAnimation));
         self._view.runner.runAction(runningAnimateAction);
+        self._view.runner.schedule(function () {
+            self._view.runner.stopAction(runningAnimateAction);
+        }, null, null,.7);
+        self._view.runner.stopAction()
     }
 });
