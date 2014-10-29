125b3b9ac0ab867521a163e5b3ec6fb0c60683a1
lvsheng <lvsheng@baidu.com>
2014-10-29 20:29:54 +0800

    debug mode

--- 跑酷游戏教程例子/src/scene/Play/PlayScene.js
+++ 跑酷游戏教程例子/src/scene/Play/PlayScene.js
@@ -5,12 +5,16 @@ var PlayScene = cc.Scene.extend({
         self._super();
         self._initPhysics();
 
+        var animationLayer = new AnimationLayer(self._space);
+
         //add three layer in the right order
         self.addChild(new BackgroundLayer());
-        self.addChild(new AnimationLayer(self._space));
+        self.addChild(animationLayer);
         self.addChild(new StatusLayer());
 
-        self.scheduleUpdate(); //TODO? for what?
+        window.animationLayer = animationLayer;
+
+        self.scheduleUpdate();
     },
     update: function (dt) {
         var self = this;
@@ -20,14 +24,14 @@ var PlayScene = cc.Scene.extend({
     _initPhysics: function () {
         var self = this;
         self._space = new cp.Space();
-        self._space.gravity = cp.v(0, -350);
+        self._space.gravity = cp.v(0, -35);
 
         //set up walls
         var ground = new cp.SegmentShape(
             self._space.staticBody,
             cp.v(0, g_groundHight),
             cp.v(MAX_INT, g_groundHight),
-            0 //厚度为0
+            200 //厚度为0
         );
         self._space.addStaticShape(ground);
     }
--- 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
+++ 跑酷游戏教程例子/src/scene/Play/layer/AnimationLayer.js
@@ -1,7 +1,6 @@
 var AnimationLayer = cc.Layer.extend({
-    _view: {
-        runner: null
-    },
+    _runner: null,
+    _physicDebugNode: null,
     _space: null,
 
     ctor:function (space) {
@@ -19,9 +18,13 @@ var AnimationLayer = cc.Layer.extend({
     _initView: function () {
         var self = this;
 
+        //debug node
+        self._physicDebugNode = new cc.PhysicsDebugNode(self._space);
+        self.addChild(self._physicDebugNode);
+
         //create the runner sprite
         var runner = new cc.PhysicsSprite(res.runner_png);
-        self._view.runner = runner;
+        self._runner = runner;
 
         //body
         var M_RUNNER = 1;
@@ -37,12 +40,12 @@ var AnimationLayer = cc.Layer.extend({
         var shape = new cp.BoxShape(runnerBody, runner.getContentSize().width, runner.getContentSize().height);
         self._space.addShape(shape);
 
-        self._view.runner.setBody(runnerBody);
+        self._runner.setBody(runnerBody);
         self.addChild(runner);
 
         //create runner animation
         var runningAnimation = myUtil.createAnimationWithPlistFileName(res.running_plist, 0.1);
         var runningAnimateAction = new cc.RepeatForever(new cc.Animate(runningAnimation));
-        self._view.runner.runAction(runningAnimateAction);
+        self._runner.runAction(runningAnimateAction);
     }
 });
