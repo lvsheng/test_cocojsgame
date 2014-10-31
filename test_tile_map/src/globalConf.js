eaa8979187ec0d22ac9bc7055777652c2c7c9deb
lvsheng <lvsheng@baidu.com>
2014-10-31 09:41:09 +0800

    save

--- test_tile_map/src/globalConf.js
+++
@@ -1,4 +0,0 @@
-var g_groundHight = 57;
-var g_runnerStartX = 80;
-
-var MAX_INT = 4294967295;
--- test_tile_map/src/scene/Menu/MenuScene.js
+++
@@ -1,7 +0,0 @@
-var MenuScene = cc.Scene.extend({
-    onEnter:function () {
-        this._super();
-
-        this.addChild(new MenuLayer());
-    }
-});
